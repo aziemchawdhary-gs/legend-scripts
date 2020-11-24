8b5eec0824f6f314941dfe7053c3bfe27327a7de
Aziem Chawdhary <aziem.chawdhary@gs.com>
2020-11-24 22:55:31 +0000

    Fix Pure IDE Light script

--- run-pure-light-ide-server.sh
+++ run-pure-light-ide-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PURELOC=$HOME/pure/legend-pure/legend-pure-ide-light
+PURELOC=$HOME/pure/legend-pure
 CONFIG_LOC=$HOME/pure/legend-pure/legend-pure-ide-light/src/main/resources/ideLightConfig.json
 
-cd $PURELOC && mvn exec:java -Dexec.mainClass="org.finos.legend.pure.ide.light.PureIDEServer" -Dexec.args="server ${CONFIG_LOC}"
+cd $PURELOC && mvnDebug -pl legend-pure-ide-light exec:java -Dexec.mainClass="org.finos.legend.pure.ide.light.PureIDEServer" -Dexec.args="server ${CONFIG_LOC}"
