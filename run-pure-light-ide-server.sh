b8266cc513e080b0af7d4b4c82ddf9753ebe20f4
Aziem Chawdhary <aziem.chawdhary@gs.com>
2021-06-05 00:28:31 +0100

    Add development options for IDE script

--- run-pure-light-ide-server.sh
+++ run-pure-light-ide-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PURELOC=$HOME/pure/legend-pure
-CONFIG_LOC=$HOME/pure/legend-pure/legend-pure-ide-light/src/main/resources/ideLightConfig.json
+PURELOC=$HOME/projects/pure/legend-pure
+CONFIG_LOC=$HOME/projects/pure/legend-pure/legend-pure-ide-light/src/main/resources/ideLightConfig.json
 
-cd $PURELOC && mvnDebug -pl legend-pure-ide-light exec:java -Dexec.mainClass="org.finos.legend.pure.ide.light.PureIDEServer" -Dexec.args="server ${CONFIG_LOC}"
+export MAVEN_OPTS="-Dpure.option.DebugPlatformCodeGen=true -Dpure.option.PlanLocal=true -Dpure.option.ExecPlan=true -Dpure.option.ShowLocalPlan=true -Dpure.option.IncludeAlloyOnlyTests=true -Dlegend.test.clientVersion=vX_X_X -Dlegend.test.serverVersion=v1 -Dlegend.test.serializationKind=json -Dlegend.test.server.host=localhost -Dlegend.test.server.port=6060"
+cd $PURELOC && mvn -pl legend-pure-ide-light exec:java -Dexec.mainClass="org.finos.legend.pure.ide.light.PureIDEServer" -Dexec.args="server ${CONFIG_LOC}"
