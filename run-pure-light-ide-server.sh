7f6b62ca3a2ad048cac1cc01a69e9752cb3c1eb3
Aziem Chawdhary <aziem.chawdhary@gs.com>
2024-03-28 15:05:55 +0000

    Update IDE launcher script

--- run-pure-light-ide-server.sh
+++ run-pure-light-ide-server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 PURELOC=$HOME/pure/legend-engine
-CONFIG_LOC=$HOME/pure/legend-engine/legend-engine-pure/legend-engine-pure-ide/legend-engine-pure-ide-light/src/main/resources/ideLightConfig.json
+CONFIG_LOC=$HOME/pure/legend-engine/legend-engine-pure/legend-engine-pure-ide/legend-engine-pure-ide-light-http-server/src/main/resources/ideLightConfig.json
 
+
+
+#export MAVEN_OPTS="-Dpure.option.DebugPlatformCodeGen=true -Dpure.option.PlanLocal=true -Dpure.option.ExecPlan=true -Dpure.option.ShowLocalPlan=true -Dpure.option.IncludeAlloyOnlyTests=true"
 export MAVEN_OPTS="-Dpure.option.DebugPlatformCodeGen=true -Dpure.option.PlanLocal=true -Dpure.option.ExecPlan=true -Dpure.option.ShowLocalPlan=true -Dpure.option.IncludeAlloyOnlyTests=true -Dlegend.test.clientVersion=vX_X_X -Dlegend.test.serverVersion=v1 -Dlegend.test.serializationKind=json -Dlegend.test.server.host=127.0.0.1 -Dlegend.test.server.port=6300"
 # export MAVEN_OPTS="-Dlegend.test.server.port=6300"
-cd $PURELOC && mvn -pl legend-engine-pure/legend-engine-pure-ide/legend-engine-pure-ide-light exec:java -Dexec.mainClass="org.finos.legend.engine.ide.PureIDELight" -Dexec.args="server ${CONFIG_LOC}"
+cd $PURELOC && mvn -pl legend-engine-pure/legend-engine-pure-ide/legend-engine-pure-ide-light-http-server exec:java -Dexec.mainClass="org.finos.legend.engine.ide.PureIDELight" -Dexec.args="server ${CONFIG_LOC}"
