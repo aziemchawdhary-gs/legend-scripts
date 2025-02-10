770db6467e7d0e10749757d85d10cedb6db31f45
Aziem Chawdhary <aziem.chawdhary@gs.com>
2025-02-10 14:34:34 +0000

    Update paths in launcher scripts

--- run-legend-engine-server.sh
+++ run-legend-engine-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ENGINELOC=$HOME/pure/legend-engine
-CONFIG_LOC=$HOME/pure/legend-engine/legend-engine-config/legend-engine-server/config/config.json
+CONFIG_LOC=$HOME/pure/legend-engine/legend-engine-config/legend-engine-server/legend-engine-server-http-server/config/config.json
 
 
 export MAVEN_OPTS="-Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n"
-cd $ENGINELOC && mvn -pl legend-engine-config/legend-engine-server exec:java -Dexec.mainClass="org.finos.legend.engine.server.Server" -Dexec.args="server ${CONFIG_LOC}"
+cd $ENGINELOC && mvn -U -pl legend-engine-config/legend-engine-server/legend-engine-server-http-server exec:java -Dexec.mainClass="org.finos.legend.engine.server.Server" -Dexec.args="server ${CONFIG_LOC}"
